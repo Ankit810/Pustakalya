735d20661193ca711f2f30710966d217620660bb
Jatin Solanki <51917666+Jatin17Solanki@users.noreply.github.com>
2020-10-01 22:26:43 +0530

    code restructured for murder mystery

---
+++ book_app/lib/model.dart
@@ -0,0 +1,25 @@
+class Book{
+  String title;
+  String author;
+  String storyLine;
+  String rating;
+  int pages;
+  String imgPath;
+  String nationality;
+  String dob;
+  int booksPubished;
+  int copiesSold;
+
+  Book({
+    this.title,
+    this.author,
+    this.storyLine,
+    this.rating,
+    this.pages,
+    this.imgPath,
+    this.nationality,
+    this.dob,
+    this.booksPubished,
+    this.copiesSold
+  });
+}
